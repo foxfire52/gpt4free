15ad6a3f742a2f211823cc4a9f399980226ff4be
foxfire52 <foxfire52@virgilio.it>
2024-11-18 23:20:29 +0100

    Update backend.py

--- g4f/gui/server/backend.py
+++ g4f/gui/server/backend.py
@@ -55,7 +55,7 @@ class Backend_Api(Api):
                 'function': self.serve_images,
                 'methods': ['GET']
             },
-            '/har': {
+            '/harcookie': {
                 'function': self.load_har,
                 'methods': ['POST']
             }
